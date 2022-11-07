b4b286c9effc7447f22240b5b1e595df29416d9c
pazicskimihaly <mihaly.pazicski@gmail.com>
2022-11-07 13:46:48 +0100

    x

--- client/src/Pages/TopEmployees.jsx
+++ client/src/Pages/TopEmployees.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-
+import { useEffect } from "react";
+import { useState } from "react";
 function TopEmployees() {
-    
-const fetchTopEmployees = (signal) => {
-    return fetch("/api/top-paid", { signal }).then((res) => res.json());
-  };
-    fetchTopEmployees()
-    return (
-        <div>
-            hahah
-        </div>
-    )
+
+    const fetchTopEmployees = (signal) => {
+        return fetch("/api/top-paid", { signal }).then((res) => res.json());
+    };
+
+    const TopEmployeeList = () => {
+        const [data, setData] = useState(null);
+
+        useEffect(() => {
+            const controller = new AbortController();
+
+            fetchTopEmployees(controller.signal)
+                .then((TopEmployees) => {
+                    setLoading(false);
+                    setData(TopEmployees);
+                })
+                .catch((error) => {
+                    if (error.name !== "AbortError") {
+                        setData(null);
+                        throw error;
+                    }
+                })
+
+            return () => controller.abort();
+        }, []);
+
+    }
 }
 
 export default TopEmployees
